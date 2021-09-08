bb8951a4cbbc1803220a59e83f36099400fd4d91
modernfuthark <53625935+modernfuthark@users.noreply.github.com>
2021-09-07 23:04:42 -0400

    [+] Added documentation

--- Assets/Scripts/InfoController.cs
+++ Assets/Scripts/InfoController.cs
@@ -4,32 +4,51 @@ using UnityEngine;
 using UnityEngine.Video;
 using UnityEngine.UI;
 
+///<summary>
+/// InfoBox controller
+///</summary>
 public class InfoController : MonoBehaviour
 {
+	// InfoBox component
 	private GameObject infoBox;
 
+	///<summary>
+	/// Fade animator
+	///</summary>
 	public Animation fadeAnimator;
 
-	public void Start()
+
+	// Unity starter function
+	void Start()
 	{
 		infoBox = gameObject.transform.GetChild(0).gameObject;
 	}
 
+	///<summary>
+	/// Google XR PointerEnter function
+	///</summary>
 	public void OnPointerEnter()
 	{
 		gameObject.GetComponent<RectTransform>().sizeDelta += new Vector2(5, 5);
 	}
 
+	///<summary>
+	/// Google XR PointerExit function
+	///</summary>
 	public void OnPointerExit()
 	{
 		gameObject.GetComponent<RectTransform>().sizeDelta -= new Vector2(5, 5);
 	}
 
+	///<summary>
+	/// Google XR PointerClick function
+	///</summary>
 	public void OnPointerClick()
 	{
         infoBox.gameObject.SetActive(!infoBox.gameObject.activeSelf);
 	}
 
+	// Disable button function, tied to a Unity button event
 	void DisableButton(string n)
 	{
 		infoBox.gameObject.SetActive(false);
